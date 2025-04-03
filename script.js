7385c8cf23d5d318a0625047fe0ab65eaa6d4291
Shapeways.io <shapewaysio@Shapewaysios-iMac-2.local>
2025-04-03 11:50:19 +0000

    Add reset button

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-const deacreaseBtn = document.querySelector(".decreaseBtn");
-const increaseBtn = document.querySelector(".increaseBtn");
+// Query Selectors
+const deacreaseBtn = document.querySelector(".decrease-btn");
+const increaseBtn = document.querySelector(".increase-btn");
+const resetBtn = document.querySelector(".reset-btn");
 let numb = document.querySelector(".numb");
 
+// Increase Function
 const increaseNumber = () => {
   let addNumb = +numb.innerHTML;
   addNumb++;
   numb.innerHTML = addNumb;
 };
 
+// Decrease Function
 const deacreaseNumber = () => {
   let addNumb = +numb.innerHTML;
   addNumb--;
   numb.innerHTML = addNumb;
 };
 
+// Reset Function
+const resetCounter = () => {
+  numb.innerHTML = 0;
+};
+
+// Event Listeners
 increaseBtn.addEventListener("click", increaseNumber);
 deacreaseBtn.addEventListener("click", deacreaseNumber);
+resetBtn.addEventListener("click", resetCounter);
