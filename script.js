490b3d2f3797b81f6339aa1c4902b5c1de236dd2
Shapeways.io <shapewaysio@Shapewaysios-iMac-2.local>
2025-04-03 12:56:10 +0000

    Add input type range

--- script.js
+++ script.js
@@ -2,28 +2,55 @@
 const deacreaseBtn = document.querySelector(".decrease-btn");
 const increaseBtn = document.querySelector(".increase-btn");
 const resetBtn = document.querySelector(".reset-btn");
+const inpRange = document.querySelector(".inp-range");
+const displayInpNum = document.querySelector(".range-num");
 let numb = document.querySelector(".numb");
+
+displayInpNum.innerHTML = +inpRange.value;
 
 // Increase Function
 const increaseNumber = () => {
-  let addNumb = +numb.innerHTML;
-  addNumb++;
-  numb.innerHTML = addNumb;
+  const inpValue = +inpRange.value;
+  const num = +numb.innerHTML;
+
+  if (inpValue > 0) {
+    const addNumb = inpValue + num;
+    numb.innerHTML = addNumb;
+  } else {
+    let addNumb = +numb.innerHTML;
+    addNumb++;
+    numb.innerHTML = addNumb;
+  }
 };
 
 // Decrease Function
 const deacreaseNumber = () => {
-  let addNumb = +numb.innerHTML;
-  addNumb--;
-  numb.innerHTML = addNumb;
+  const inpValue = +inpRange.value;
+  const num = +numb.innerHTML;
+
+  if (inpValue > 0) {
+    const descreaseNum = num - inpValue;
+    numb.innerHTML = descreaseNum;
+  } else {
+    let addNumb = +numb.innerHTML;
+    addNumb--;
+    numb.innerHTML = addNumb;
+  }
 };
 
 // Reset Function
 const resetCounter = () => {
   numb.innerHTML = 0;
+  inpRange.value = 0;
+  displayInpNum.innerHTML = 0;
+};
+
+const changeInputValue = () => {
+  displayInpNum.innerHTML = +inpRange.value;
 };
 
 // Event Listeners
 increaseBtn.addEventListener("click", increaseNumber);
 deacreaseBtn.addEventListener("click", deacreaseNumber);
 resetBtn.addEventListener("click", resetCounter);
+inpRange.addEventListener("input", changeInputValue);
